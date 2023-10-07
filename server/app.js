2335500e1affedba57e2373d6d746692196cfb1c
rybak2Z <rybak2Z.000@gmail.com>
2023-10-07 20:34:20 +0200

    Serve react files

--- server/app.js
+++ server/app.js
@@ -11,6 +11,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../client/dist')))
+app.use(express.static(path.join(__dirname, '../client/public')))
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);

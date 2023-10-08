a84fdbffee8d02ab650626097e3a78b959c1bad9
Felix Fischer <felixfischer0204@gmail.com>
2023-10-08 12:49:08 +0200

    Set up database connection

--- server/app.js
+++ server/app.js
@@ -1,9 +1,21 @@
+require("dotenv").config();
+
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const mongoose = require("mongoose");
 
 const apiRouter = require("./routes/api");
+
+connectToDb().catch((err) => console.log(err));
+async function connectToDb() {
+  mongoose.set("strictQuery", false);
+  const mongoDbUri = process.env.MONGODB_URI;
+  const databaseName = process.env.DB_NAME;
+  await mongoose.connect(mongoDbUri, { dbName: databaseName });
+  console.log("Successfully connected to mongodb");
+}
 
 const app = express();
 

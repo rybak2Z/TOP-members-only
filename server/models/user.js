c71a2c23acf9844340af6f45afb87a7c2dd991df
Felix Fischer <felixfischer0204@gmail.com>
2023-10-09 15:43:08 +0200

    Implement admin status

--- server/models/user.js
+++ server/models/user.js
@@ -6,6 +6,7 @@ const UserSchema = new Schema({
   username: { type: String, required: true, maxLength: 16 },
   password: { type: String, required: true, maxLength: 128 },
   isMember: { type: Boolean, required: true },
+  isAdmin: { type: Boolean, required: true },
 });
 
 module.exports = mongoose.model("User", UserSchema);

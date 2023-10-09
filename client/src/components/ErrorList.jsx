6a22e07085a333171b7ec52eb1debf5f58a13f91
rybak2Z <rybak2Z.000@gmail.com>
2023-10-09 22:17:44 +0200

    Improve error list styling

--- client/src/components/ErrorList.jsx
+++ client/src/components/ErrorList.jsx
@@ -1,7 +1,8 @@
 function ErrorList({ errors }) {
   const errorElements = errors.map((err, idx) => <li key={idx}>{err}</li>);
+  const list = <ul>{errors.length > 0 && errorElements}</ul>;
 
-  return <>{errors.length > 0 && errorElements}</>;
+  return errors.length > 0 ? list : null;
 }
 
 export default ErrorList;

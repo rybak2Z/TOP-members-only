2f1695fd7695aee4200d80186c10f4fa97b6f72a
rybak2Z <rybak2Z.000@gmail.com>
2023-10-10 15:40:57 +0200

    Improve main page styling

--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -63,14 +63,30 @@ function MainPage({ setUser }) {
     );
   }
 
+  const accountStatus = user.isAdmin
+    ? "Admin"
+    : user.isMember
+    ? "Club Member"
+    : "Regular Member";
   return (
     <>
-      <h1>Welcome, {user.username}!</h1>
-      <a href="/api/log-out">Log out</a>
-      <Link to="join-club">Become a club member</Link>
-      <Link to="create-message">Create a new message</Link>
+      <h1 style={{ marginBottom: 0 }}>Welcome, {user.username}!</h1>
+      <h2>Your status: {accountStatus}</h2>
+      <div class="button-list">
+        <Link to="join-club" class="thin-padding">
+          Become a club member
+        </Link>
+        <a href="/api/log-out" class="thin-padding">
+          Log out
+        </a>
+      </div>
       <hr />
-      <MessageList />
+      <div>
+        <Link to="create-message" id="new-message-button">
+          Create a new message
+        </Link>
+        <MessageList />
+      </div>
     </>
   );
 }

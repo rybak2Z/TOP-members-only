256d1ae9e3c1a512427964f13d195b15ca58b377
Felix Fischer <felixfischer0204@gmail.com>
2023-10-10 20:23:41 +0200

    Refactor: Make login form its own component

---
+++ client/src/components/LoginForm.jsx
@@ -0,0 +1,44 @@
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import useForm from "../hooks/useForm";
+import ErrorList from "../components/ErrorList";
+
+function LoginForm({ setUser }) {
+  const [errors, setErrors] = useState([]);
+  const handleSubmit = useForm(responseHandler);
+
+  async function responseHandler(response) {
+    if (response.status === 400 || response.status === 401) {
+      setErrors(["Incorrect username or password."]);
+      return;
+    }
+    if (!response.ok) {
+      throw new Error(`Unrecognized error: ${response.status}`);
+    }
+    const data = await response.json();
+    setUser(data); // { username, isMember, isAdmin }
+    setErrors([]);
+  }
+
+  return (
+    <>
+      <form method="POST" action="/api/log-in" onSubmit={handleSubmit}>
+        <label for="username">Username</label>
+        <input type="text" name="username" id="username" />
+        <label for="password">Password</label>
+        <input type="text" name="password" id="password" />
+        <button type="submit">Submit</button>
+      </form>
+      <ErrorList errors={errors} />
+      <hr />
+      <div class="flex">
+        <span>Don't have an account?</span>
+        <Link to="sign-up" class="thin-padding">
+          Sign up
+        </Link>
+      </div>
+    </>
+  );
+}
+
+export default LoginForm;
--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -1,56 +1,24 @@
-import { useState, useContext } from "react";
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { UserContext } from "../App";
-import useForm from "../hooks/useForm";
 import MessageList from "../components/MessageList";
-import ErrorList from "../components/ErrorList";
+import LoginForm from "../components/LoginForm";
 
 function MainPage({ setUser }) {
   const user = useContext(UserContext);
-  const [errors, setErrors] = useState([]);
-  const handleSubmit = useForm(responseHandler);
 
-  async function responseHandler(response) {
-    if (response.status === 400 || response.status === 401) {
-      setErrors(["Incorrect username or password."]);
-      return;
-    }
-    if (!response.ok) {
-      throw new Error(`Unrecognized error: ${response.status}`);
-    }
-    const data = await response.json();
-    setUser(data); // { username, isMember, isAdmin }
-    setErrors([]);
+  let accountStatus;
+  if (user?.isAdmin) {
+    accountStatus = "Admin";
+  } else if (user?.isMember) {
+    accountStatus = "Club Member";
+  } else {
+    accountStatus = "Regular Member";
   }
 
-  if (user === null) {
-    return (
-      <>
-        <form method="POST" action="/api/log-in" onSubmit={handleSubmit}>
-          <label for="username">Username</label>
-          <input type="text" name="username" id="username" />
-          <label for="password">Password</label>
-          <input type="text" name="password" id="password" />
-          <button type="submit">Submit</button>
-        </form>
-        <ErrorList errors={errors} />
-        <hr />
-        <div class="flex">
-          <span>Don't have an account?</span>
-          <Link to="sign-up" class="thin-padding">
-            Sign up
-          </Link>
-        </div>
-      </>
-    );
-  }
-
-  const accountStatus = user.isAdmin
-    ? "Admin"
-    : user.isMember
-    ? "Club Member"
-    : "Regular Member";
-  return (
+  return user === null ? (
+    <LoginForm setUser={setUser} />
+  ) : (
     <>
       <h1 style={{ marginBottom: 0 }}>Welcome, {user.username}!</h1>
       <h2>Your status: {accountStatus}</h2>

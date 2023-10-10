ab82104750339fd23fe9a250aa2fb4aed8d3d952
Felix Fischer <felixfischer0204@gmail.com>
2023-10-10 19:23:10 +0200

    Sort messages by newest

--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -4,12 +4,10 @@ import { UserContext } from "../App";
 function Message({ text, username, date, onDelete }) {
   const user = useContext(UserContext);
 
-  const formattedDate = date ? new Date(date).toLocaleString() : null;
-
   return (
     <li class="message">
       <h1>Author: {username ?? "(hidden for non club members)"}</h1>
-      <h2>Date: {formattedDate ?? "(hidden for non club members)"}</h2>
+      <h2>Date: {date?.toLocaleString() ?? "(hidden for non club members)"}</h2>
       <p>{text}</p>
       {user.isAdmin && (
         <button class="thin-padding button-secondary" onClick={onDelete}>

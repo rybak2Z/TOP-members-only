37a0154f8602f6df267e2472dd66170d8e9abb80
Felix Fischer <felixfischer0204@gmail.com>
2023-10-10 21:32:51 +0200

    Combine api message routes

--- server/routes/api.js
+++ server/routes/api.js
@@ -4,9 +4,11 @@ const { post_sign_up } = require("../controllers/sign-up");
 const { post_log_in } = require("../controllers/log-in");
 const { get_log_out } = require("../controllers/log-out");
 const { post_join_club } = require("../controllers/join-club");
-const { post_create_message } = require("../controllers/create-message");
-const { get_messages } = require("../controllers/messages");
-const { delete_message } = require("../controllers/message");
+const {
+  get_messages,
+  post_create_message,
+  delete_message,
+} = require("../controllers/message");
 
 const router = express.Router();
 
@@ -18,9 +20,9 @@ router.get("/log-out", get_log_out);
 
 router.post("/join-club", post_join_club);
 
-router.post("/create-message", post_create_message);
+router.post("/message/", post_create_message);
 
-router.get("/messages", get_messages);
+router.get("/message/all", get_messages);
 
 router.delete("/message/:id", delete_message);
 

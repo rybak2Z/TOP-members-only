cd0140e8b21f9ee1d525c19314a58792031ccb3f
rybak2Z <rybak2Z.000@gmail.com>
2023-10-11 17:36:50 +0200

    Set up vite proxy server

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const API_BASE_URL = "http://localhost:3000";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: API_BASE_URL,
+      },
+    },
+  },
 });
